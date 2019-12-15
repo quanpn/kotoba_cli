fe37b348cfd66607dd8d89a3e81670023f16f34f
Pham Ngoc Quan <quanpn82@gmail.com>
2019-12-15 03:40:52 +0000

    backup api and arg sample

--- bin/index.js
+++ bin/index.js
@@ -1,58 +1,13 @@
 #!/usr/bin/env node
-
-const chalk = require("chalk");
-const boxen = require("boxen");
 const yargs = require("yargs");
-const axios = require("axios");
-
-
-const boxenOptions = {
- padding: 1,
- margin: 1,
- borderStyle: "round",
- borderColor: "green",
- backgroundColor: "#555555"
-};
-
-
 
 const options = yargs
- .usage("Usage: -n <name>")
- .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
- .option("s", { alias: "search", describe: "Search term", type: "string" })
+ .usage("Usage:  [command] [param]")
+ .option("e", { alias: "export", describe: "Export a collection", type: "string", demandOption: true })
  .argv;
 
-const greeting = `Hello, ${options.name}!`;
-
-console.log(greeting);
-
-const greetingBox = chalk.white.bold(greeting);
-const msgBox = boxen( greetingBox, boxenOptions );
-
-console.log(msgBox);
-
-if (options.search) {
- console.log(`Searching for jokes about ${options.search}...`)
+if (options.export) {
+ console.log(`Doing export ${options.export}...`)
 } else {
- console.log("Here's a random joke for you:");
+ console.log("There is not any command...Do nothing");
 }
-
-
-
-const url = options.search ? `https://icanhazdadjoke.com/search?term=${escape(options.search)}` : "https://icanhazdadjoke.com/";
-
-
-axios.get(url, { headers: { Accept: "application/json" } })
- .then(res => {
-   if (options.search) {
-     // if searching for jokes, loop over the results
-     res.data.results.forEach( j => {
-       console.log("\n" + j.joke);
-     });
-     if (res.data.results.length === 0) {
-       console.log("no jokes found :'(");
-     }
-   } else {
-     console.log(res.data.joke);
-   }
- });
