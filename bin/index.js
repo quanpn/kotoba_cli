77d2ea85bef1511a65eb0664224f568939b863e5
Pham Ngoc Quan <quanpn82@gmail.com>
2019-12-16 07:03:20 +0700

    Add firebase-admin

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const yargs = require("yargs");
+var admin = require("firebase-admin");
 
 const options = yargs
  .usage("Usage:  [command] [param]")
@@ -11,3 +12,10 @@ if (options.export) {
 } else {
  console.log("There is not any command...Do nothing");
 }
+
+var serviceAccount = require("./cert/kotoba-1042b-firebase-adminsdk-pspi7-bedf63e907.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://kotoba-1042b.firebaseio.com"
+});
