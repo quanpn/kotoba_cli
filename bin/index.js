4896fa9389d81cf759f79ae0ad9faba38974e883
Pham Ngoc Quan <quanpn82@gmail.com>
2019-12-16 06:39:55 +0000

    Authenticate with admin privileges

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,7 @@ if (options.export) {
  console.log("There is not any command...Do nothing");
 }
 
-var serviceAccount = require("cert/kotoba-1042b-firebase-adminsdk-pspi7-bedf63e907.json");
+var serviceAccount = "cert/kotoba-1042b-firebase-adminsdk-pspi7-bedf63e907.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
